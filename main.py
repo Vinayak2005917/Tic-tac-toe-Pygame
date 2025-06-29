b8433d91265088c09dcaf39ef9b3df92ec3970bd
Vinayak <145529911+Vinayak2005917@users.noreply.github.com>
2025-06-29 12:29:05 +0530

    loaded Cross and Circle and made the positions table

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ width, height = 720,720
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pygame Window")
 
+#loading images
+board = pygame.image.load('assets/main board.jpg')
+#board = board.get_rect()
+
+
 # Main loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    
 
 
     window.fill((255, 255, 255))
