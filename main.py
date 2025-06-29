afb984a779af84415426d487a86cc99f8a790c75
Vinayak <145529911+Vinayak2005917@users.noreply.github.com>
2025-06-29 12:04:56 +0530

    added assets and set the bg as white

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 import pygame
+from pygame.locals import *
+pygame.init()
+
+# Set up the display
+width, height = 720,720
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pygame Window")
+
+# Main loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    window.fill((255, 255, 255))
+    pygame.display.update()
+
+
+
+
+
+
+
+
+# Quit Pygame
+pygame.quit()
+exit()
